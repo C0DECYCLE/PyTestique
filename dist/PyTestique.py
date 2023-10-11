2d4c30780fd0be313d99679cf1290e37bb2f87af
Patertuck <patric.brandao@hispeed.ch>
2023-10-11 14:02:33 +0200

    FRAMEWORK-FEATURE: Add analytics

--- dist/PyTestique.py
+++ dist/PyTestique.py
@@ -1,3 +1,24 @@
+import time
+from typing import Dict, Optional
+
+
+class PyTestiqueAnalytics:
+    __timeDict: Dict[str, float]
+
+    def __init__(self, name: str) -> None:
+        self.__timeDict = {}
+
+    def timeStart(self, name: str) -> None:
+        self.__timeDict[name] = time.time()
+
+    def timeStop(self, name: str) -> Optional[float]:
+        if name in self.__timeDict:
+            return time.time() - self.__timeDict[name]
+        return None
+
+
 class PyTestique:
+    analytics: PyTestiqueAnalytics
+
     def __init__(self):
-        pass
+        self.analytics = PyTestiqueAnalytics()
