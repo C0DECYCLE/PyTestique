80d27ba8b528ed2a869b2f39f5522edea52eb2d0
Patertuck <115107324+Patertuck@users.noreply.github.com>
2023-10-11 14:11:05 +0200

    FRAMEWORK-FEATURE: Add analytics (#17)
    * FRAMEWORK-FEATURE: Add analytics
    
    * FRAMEWORK-FEATURE: Change naming without type
    
    ---------
    
    Co-authored-by: Noah Bussinger <18073837+C0DECYCLE@users.noreply.github.com>

--- dist/PyTestique.py
+++ dist/PyTestique.py
@@ -1,6 +1,24 @@
-from typing import List, Dict
+import time
+from typing import List, Dict, Optional
+
+
+class PyTestiqueAnalytics:
+    __times: Dict[str, float]
+
+    def __init__(self, name: str) -> None:
+        self.__times = {}
+
+    def timeStart(self, name: str) -> None:
+        self.__times[name] = time.time()
+
+    def timeStop(self, name: str) -> Optional[float]:
+        if name in self.__times:
+            return time.time() - self.__times[name]
+        return None
 
 
 class PyTestique:
+    analytics: PyTestiqueAnalytics
+
     def __init__(self, cliArgs: List[str], globalContext: Dict[str, any]) -> None:
-        pass
+        self.analytics = PyTestiqueAnalytics()
