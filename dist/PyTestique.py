7f689036f84a01931d8c5ba4997ae40b2c7d8e86
Patertuck <patric.brandao@hispeed.ch>
2023-10-11 14:06:14 +0200

    FRAMEWORK-FEATURE: Change naming without type

--- dist/PyTestique.py
+++ dist/PyTestique.py
@@ -3,17 +3,17 @@ from typing import Dict, Optional
 
 
 class PyTestiqueAnalytics:
-    __timeDict: Dict[str, float]
+    __times: Dict[str, float]
 
     def __init__(self, name: str) -> None:
-        self.__timeDict = {}
+        self.__times = {}
 
     def timeStart(self, name: str) -> None:
-        self.__timeDict[name] = time.time()
+        self.__times[name] = time.time()
 
     def timeStop(self, name: str) -> Optional[float]:
-        if name in self.__timeDict:
-            return time.time() - self.__timeDict[name]
+        if name in self.__times:
+            return time.time() - self.__times[name]
         return None
 
 
