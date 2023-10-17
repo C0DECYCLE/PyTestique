974b160a2ae405a194bc81300e0a7909092db74b
Patertuck <patic.brandao@hispeed.ch>
2023-10-17 18:47:12 +0200

    USER-FEATURE: Add tests for file_manager.py functions

--- dist/run_test.py
+++ dist/run_test.py
@@ -1,5 +1,7 @@
+import os
 import sys
 from dist.PyTestique import PyTestique, PyTestiqueAsserts
+from file_manager import read_file, create_file, delete_file
 
 
 def test_foo() -> None:
@@ -33,6 +35,94 @@ def test_foobarfoo() -> None:
 def teardown_foobarfoo() -> None:
     print("teardown foobarfoo!")
     raise Exception("Teardown foobarfoo exception.")
+
+
+#################### read file tests #########################
+
+
+def test_read_file_not_exist():
+    text = read_file("test_file_read_text.txt")
+    PyTestiqueAsserts.assertIsNone(text)
+
+
+def setup_read_file_content():
+    file_name = "test_file_read_content.txt"
+    with open(file_name, "w") as file:
+        file.write("This is a test file.")
+
+
+def test_read_file_content():
+    PyTestiqueAsserts.assertEqual(
+        "This is a test file.", read_file("test_file_read_content.txt")
+    )
+
+
+def teardown_read_file_content():
+    file_name = "test_file_read_content.txt"
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+
+#################### create file tests #########################
+def test_create_file_true():
+    file_name = "test_file_create.txt"
+    returned = create_file(file_name)
+
+    if os.path.exists(file_name):
+        PyTestiqueAsserts.assertTrue(returned)
+
+
+def teardown_create_file_true():
+    file_name = "test_file_create.txt"
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+
+def test_create_file_content():
+    file_name = "test_file_create_content.txt"
+    text = "I have created a file"
+    create_file(file_name, text)
+
+    if os.path.exists(file_name):
+        with open(file_name, "r") as file:
+            content = file.read()
+        PyTestiqueAsserts.assertEqual(content, text)
+
+
+def teardown_create_file_content():
+    file_name = "test_file_create_content.txt"
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+
+#################### delete file tests #########################
+
+
+def setup_delete_file_exist():
+    file_name = "test_file_delete_exist.txt"
+    with open(file_name, "w") as file:
+        file.write("This is a test file.")
+
+
+def test_delete_file_exist():
+    file_name = "test_file_delete_exist.txt"
+    delete_file(file_name)
+
+    if os.path.exists(file_name):
+        PyTestiqueAsserts.assertTrue(False)
+
+
+def teardown_delete_file_exist():
+    file_name = "test_file_delete_exist.txt"
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+
+def test_delete_file_not_exist():
+    file_name = "test_file_delete_not_exist.txt"
+    deleted = delete_file(file_name)
+
+    PyTestiqueAsserts.assertFalse(deleted)
 
 
 PyTestique(sys.argv, globals())
